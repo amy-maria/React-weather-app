ce0402af9a7275b3d5ccbae3d99bdc4e183c7b9b
amy-maria <122695916+amy-maria@users.noreply.github.com>
2023-04-16 15:21:36 -0400

    Changes made to format date and degrees

---
+++ React-weather-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Search from "./Search.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Search />
+    </div>
+  );
+}
